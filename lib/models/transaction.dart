0eeef3d99949333d71ae53463193a850a161bf4f
tuanvu9981 <tuanvutruonglacnhatsinh0801@gmail.com>
2023-04-06 23:33:02 +0700

    feat: complete home main page

---
+++ lib/models/transaction.dart
@@ -0,0 +1,53 @@
+class Transaction {
+  String? imageUrl;
+  String? name;
+  String? time;
+  String? fee;
+  bool? buy;
+
+  Transaction({
+    this.imageUrl,
+    this.name,
+    this.time,
+    this.fee,
+    this.buy,
+  });
+}
+
+final transactions = <Transaction>[
+  Transaction(
+    imageUrl: 'assets/images/salary.jpg',
+    name: 'April salary',
+    time: '2023 April 20th',
+    fee: '+ 2000 \$',
+    buy: false,
+  ),
+  Transaction(
+    imageUrl: 'assets/images/hamburger.jpg',
+    name: 'Hamburger',
+    time: '2023 April 20th',
+    fee: '- 8.5 \$',
+    buy: true,
+  ),
+  Transaction(
+    imageUrl: 'assets/images/tra-sen-vang.png',
+    name: 'Highland coffee',
+    time: '2023 April 22th',
+    fee: '- 3.25 \$',
+    buy: true,
+  ),
+  Transaction(
+    imageUrl: 'assets/images/salary.jpg',
+    name: 'Scholarship',
+    time: '2023 April 22th',
+    fee: '+ 850 \$',
+    buy: false,
+  ),
+  Transaction(
+    imageUrl: 'assets/images/beef-wellington.jpg',
+    name: 'Beef Wellington',
+    time: '2023 April 20th',
+    fee: '- 10.25 \$',
+    buy: true,
+  ),
+];
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,87 +1,284 @@
+import 'package:finance_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWeight = MediaQuery.of(context).size.width;
-
-    return Scaffold(
-      body: SafeArea(
-        child: Stack(
+  Widget _buildHeader(double screenHeight, double screenWidth) {
+    return Stack(
+      children: [
+        Column(
           children: [
-            Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: screenHeight * 0.28,
-                  decoration: const BoxDecoration(
-                    color: Color(0xff368983),
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20.0),
-                      bottomRight: Radius.circular(20.0),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Container(
+              width: double.infinity,
+              height: screenHeight * 0.28,
+              decoration: const BoxDecoration(
+                color: Color(0xff368983),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20.0),
+                  bottomRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Good afternoon',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16.0,
-                                color: Color.fromARGB(255, 224, 223, 223),
-                              ),
-                            ),
-                            Text(
-                              'Enjelin Morgeara',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 20.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
+                      children: const [
+                        Text(
+                          'Good afternoon',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16.0,
+                            color: Color.fromARGB(255, 224, 223, 223),
+                          ),
                         ),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(7.0),
-                          child: Container(
-                            width: 40.0,
-                            height: 40.0,
-                            color: const Color.fromRGBO(250, 250, 250, 0.1),
-                            child: const Icon(
-                              Icons.notification_add_outlined,
-                              size: 30.0,
-                              color: Colors.white,
-                            ),
+                        Text(
+                          'Enjelin Morgeara',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20.0,
+                            color: Colors.white,
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ),
-              ],
-            ),
-            Positioned(
-              top: screenHeight * 0.28 * 0.7,
-              left: screenWeight * 0.13,
-              child: Container(
-                height: screenHeight * 0.2,
-                width: screenWeight * 0.74,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 47, 125, 121),
-                  borderRadius: BorderRadius.circular(15.0),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(7.0),
+                      child: Container(
+                        width: 40.0,
+                        height: 40.0,
+                        color: const Color.fromRGBO(250, 250, 250, 0.1),
+                        child: const Icon(
+                          Icons.notification_add_outlined,
+                          size: 30.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
+          ],
+        ),
+        Positioned(
+          top: screenHeight * 0.28 * 0.7,
+          left: screenWidth * 0.13,
+          child: Container(
+            height: screenHeight * 0.2,
+            width: screenWidth * 0.74,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 47, 125, 121),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromRGBO(47, 125, 121, 0.3),
+                  offset: Offset(0, 6),
+                  blurRadius: 7.5,
+                  spreadRadius: 4.0,
+                )
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'Total Balance',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 5.0),
+
+                  // Total money
+                  const Text(
+                    '\$ 2977',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0,
+                      color: Colors.white,
+                    ),
+                  ),
+
+                  // Income & Expense
+                  const SizedBox(height: 15.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.add_circle_outline_sharp,
+                            color: Colors.white,
+                            size: 25.0,
+                          ),
+                          SizedBox(width: 5.0),
+                          Text(
+                            'Income',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.5,
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.remove_circle_outline_sharp,
+                            color: Colors.white,
+                            size: 25.0,
+                          ),
+                          SizedBox(width: 5.0),
+                          Text(
+                            'Expense',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.5,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+
+                  // Income & Expense
+                  const SizedBox(height: 6.0),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text(
+                          '\$ 1400',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '\$ 249.5',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: screenHeight * (0.28 * 0.7 + 0.2),
+                child: _buildHeader(screenHeight, screenWidth),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15.0,
+                  vertical: 20.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      'Transaction History',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'See all',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: transactions.length,
+                (context, index) {
+                  return ListTile(
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: Image.asset(
+                        transactions[index].imageUrl!,
+                        height: 65.0,
+                        width: 65.0,
+                      ),
+                    ),
+                    title: Text(
+                      transactions[index].name!,
+                      style: const TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    subtitle: Text(
+                      transactions[index].time!,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    trailing: Text(
+                      transactions[index].fee!,
+                      style: TextStyle(
+                        color: transactions[index].buy == true
+                            ? Colors.red
+                            : Colors.green,
+                        fontSize: 17.5,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
